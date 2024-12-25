b0acabda6a8f5bda53c27e68f06e7a5eb0013f75
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-12-25 20:56:52 +0530

    updateb expert section

--- src/components/experts/ExpertButton.jsx
+++ src/components/experts/ExpertButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { IoIosArrowForward } from "react-icons/io";
 
 const ExpertButton = () => {
   return (
-    <div>
-        <button className='bg-[#DBE3DC] text-dark px-6 py-2 rounded-lg text-[1.5rem] border-dark border'>Find more experts</button>
+    <div className='flex items-center gap-3 bg-[#DBE3DC] text-dark px-6 py-2 rounded-lg text-[1.5rem] border-dark border '>
+        <button className=''>Find more experts</button>
+        <span className='text-[18px]' ><IoIosArrowForward /></span>
     </div>
   )
 }
